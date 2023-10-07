b151b0d021554ca3588c89248efb1937d3ff6890
CamiloVelezP <camilo.velezp@udea.edu.co>
2023-10-07 12:00:04 -0500

    ya no error

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 // test.js
-const add = require('./add.js');
+const add = require('./suma.js');
 
 test('Adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+    expect(add(1, 2)).toBe(4);
 });
